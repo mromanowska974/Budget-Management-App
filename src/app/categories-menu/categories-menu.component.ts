960977a722ffcd68926ed913ba06b3249f531ea5
mromanowska974 <m.romanowska974@gmail.com>
2024-07-05 09:22:58 +0100

    Reconstruction of Main Page
    The structure of the Main Page has been reconstructed in order to implement Categories Menu.

---
+++ src/app/categories-menu/categories-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categories-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './categories-menu.component.html',
+  styleUrl: './categories-menu.component.css'
+})
+export class CategoriesMenuComponent {
+
+}
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -3,6 +3,16 @@ import { WidgetDirective } from '../directives/widget.directive';
 import { ButtonDirDirective } from '../directives/button-dir.directive';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+<<<<<<< Updated upstream
+=======
+import { User } from '../models/user.interface';
+import { Profile } from '../models/profile.interface';
+import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
+import { ProfileAuthService } from '../services/profile-auth.service';
+import { LocalStorageService } from '../services/local-storage.service';
+import { CategoriesMenuComponent } from '../categories-menu/categories-menu.component';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-main-page',
@@ -10,6 +20,7 @@ import { AuthService } from '../services/auth.service';
   imports: [
     WidgetDirective,
     ButtonDirDirective,
+    CategoriesMenuComponent,
 
     RouterModule
   ],
